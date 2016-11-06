13cfe2cd87ce4e058a179233e88bc624fbb32ae6
Gusrot <gnrotela@yahoo.com.ar>
2016-11-06 15:32:29 -0300

    serializacion

---
+++ CentralTelefonica/CentralitaSerializacion/ISerializable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CentralitaSerializacion
+{
+    interface ISerializable
+    {
+        bool DeSerializarse();
+        bool Serializarse();
+
+        string RutaDeArchivo { get; set; }
+    }
+}
